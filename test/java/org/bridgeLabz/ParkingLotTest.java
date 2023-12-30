ad859e43fd9b6a4b3a665973a6abf7160e2de06c
VivekRanjan2002 <lit2020047@iiitl.ac.in>
2023-12-30 12:20:43 +0530

    Added: test cases for remove to ParkingLot

--- test/java/org/bridgeLabz/ParkingLotTest.java
+++ test/java/org/bridgeLabz/ParkingLotTest.java
@@ -11,6 +11,15 @@ class ParkingLotTest {
         Assertions.assertTrue(parkingLot.checkIfPark(car));
         Assertions.assertEquals(1,parkingLot.getcurrCapacity());
     }
+    //UC2
+    void givenCar_removeToParkingLot_return_Boolean(){
+        ParkingLot parkingLot= new ParkingLot();
+        Car car1= new Car("car1");
+        Car car2 = new Car("car2");
+        Assertions.assertFalse(parkingLot.checkIfUnpark(car2));
+        parkingLot.checkIfPark(car1);
+        Assertions.assertTrue(parkingLot.checkIfUnPark(car1));
+    }
 
 
 }
