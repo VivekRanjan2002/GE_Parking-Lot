ab23b39c7afff3a9f8b63317ea9451b5cb69e97c
VivekRanjan2002 <lit2020047@iiitl.ac.in>
2023-12-30 12:34:05 +0530

    Added: logic for removing to parkingLot test case

--- main/java/org/bridgeLabz/ParkingLot.java
+++ main/java/org/bridgeLabz/ParkingLot.java
@@ -1,15 +1,29 @@
 package org.bridgeLabz;
 
+import java.util.ArrayList;
+
 public class ParkingLot {
+    private ArrayList<Car> parkingList;
     private int currCapacity;
-    public ParkingLot(){};
+    public ParkingLot(){
+        parkingList= new ArrayList();
+        currCapacity=0;
+    };
     public int getcurrCapacity() {
         return currCapacity;
     }
     // check if car can be parked or not by flipping coin
     public boolean checkIfPark(Car car) {
-        if(Math.random()<0.5) return false;
         currCapacity++;
+        parkingList.add(car);
         return true;
+    }
+    // remove car from parkinglot if it is parked
+    public boolean checkIfUnpark(Car car) {
+        if(parkingList.size() >0 && parkingList.contains(car)){
+            parkingList.remove(car);
+            return true;
+        }
+        return false;
     }
 }
