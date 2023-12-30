5d2546dcc05bd44faae0b640b21b7f3d670902ec
VivekRanjan2002 <lit2020047@iiitl.ac.in>
2023-12-30 15:20:09 +0530

    Added: logic for checking if parkinglot full by AirportSecurity

--- main/java/org/bridgeLabz/AirportSecurity.java
+++ main/java/org/bridgeLabz/AirportSecurity.java
@@ -1,2 +1,12 @@
-package org.bridgeLabz;public class AirportSecurity {
+package org.bridgeLabz;
+
+public class AirportSecurity {
+    public static  boolean carsReqforParkingLotFull(int val) {
+        if(val==0){
+            System.out.println("Redirecting my Security Staff ");
+            return true;
+        }
+        return false;
+    }
+
 }
--- main/java/org/bridgeLabz/ParkingLot.java
+++ main/java/org/bridgeLabz/ParkingLot.java
@@ -3,32 +3,33 @@ package org.bridgeLabz;
 import java.util.ArrayList;
 
 public class ParkingLot {
-    private int maxcapacity=10;
-    private ArrayList<Car> parkingList;
-    private int currCapacity;
+    private int maxcapacity=2;
+    private ArrayList<String> parkingList=new ArrayList<>();
+    private int currCapacity=0;
     public ParkingLot(){
-        parkingList= new ArrayList();
-        currCapacity=0;
     };
     public int getcurrCapacity() {
         return currCapacity;
     }
     // check if car can be parked or not by flipping coin
     public boolean checkIfPark(Car car) {
-        currCapacity++;
-        parkingList.add(car);
-        return true;
+        if(currCapacity<maxcapacity && !parkingList.contains(car.name)) {
+            currCapacity++;
+            parkingList.add(car.name);
+            return true;
+        }
+        return false;
     }
     // remove car from parkinglot if it is parked
     public boolean checkIfUnpark(Car car) {
-        if(parkingList.size() >0 && parkingList.contains(car)){
-            parkingList.remove(car);
+        if(parkingList.size() >0 && parkingList.contains(car.name)){
+            parkingList.remove(car.name);
             return true;
         }
         return false;
     }
 
-    public int carsReqForFullCapacity() {
+    public  int carsReqForFullCapacity() {
         int val= Math.max(0,maxcapacity-currCapacity);
         System.out.println(val+ " more cars req to full the ParkingLot..");
         return val;
